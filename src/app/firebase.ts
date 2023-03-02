0ef3e57420426782aa4b02911b0c94770da17051
Xilerth <xilerth@gmail.com>
2023-03-03 00:27:30 +0100

    first push

---
+++ src/app/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfs7yOB_a0whKO9IDJc72y0VWQhkCaVQU",
+  authDomain: "lifes-cost.firebaseapp.com",
+  projectId: "lifes-cost",
+  storageBucket: "lifes-cost.appspot.com",
+  messagingSenderId: "964259127980",
+  appId: "1:964259127980:web:72ad99ad9eb9cab348b509"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
